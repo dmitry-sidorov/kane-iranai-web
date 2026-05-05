170ebb5f144fd0a532e16504830ac76d0b594d36
dmitry-sidorov <jimmaggot@mail.ru>
2026-05-05 21:43:33 +0300

    Add local api proxy path

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,6 +3,14 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5544',
+        changeOrigin: true,
+      },
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
